c3fba8ad79f0fc9ae405e1a22b946db24af9a9cb
Julian Arce <juarce@itba.edu.ar>
2023-10-12 22:10:04 -0300

    chore: enable transaction management

--- webapp/src/main/java/ar/juarce/webapp/App.java
+++ webapp/src/main/java/ar/juarce/webapp/App.java
@@ -3,6 +3,7 @@ package ar.juarce.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  *
@@ -17,6 +18,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan(basePackages = {
         "ar.juarce.models"
 })
+@EnableTransactionManagement
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
