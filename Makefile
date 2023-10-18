f7087aab46f8a4768deb015f45e5a261cdf28ed6
Julián Arce <juarce@itba.edu.ar>
2023-10-17 21:18:42 -0300

    fix: remove "" from makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 # Database variables
-container = "postgres"
-user = "user"
-password = "123123123"
-database = "java-api-template"
+container = postgres
+user = user
+password = 123123123
+database = java_api_template
 
 
 # Application Commands
@@ -29,10 +29,10 @@ create_db:
 	@if [ "$(container)" = "postgres" ]; then echo "Using default container name: 'postgres'."; fi
 	@if [ "$(user)" = "user" ]; then echo "Using default user name: 'user'."; fi
 	@if [ "$(password)" = "123123123" ]; then echo "Using default container name: '123123123'."; fi
-	@if [ "$(database)" = "java-api-template" ]; then echo "Using default container name: 'java-api-template'."; fi
+	@if [ "$(database)" = "java-api-template" ]; then echo "Using default container name: 'java_api_template'."; fi
 
 	docker run --name $(container) -e POSTGRES_PASSWORD=$(password) -e POSTGRES_USER=$(user) -p 5432:5432 -d postgres
-	sleep 1  # Wait for the PostgreSQL container to start (you can adjust this as needed)
+	sleep 5  # Wait for the PostgreSQL container to start (you can adjust this as needed)
 	docker exec -it $(container) psql -U $(user) -c 'CREATE DATABASE "$(database)" WITH ENCODING "UTF-8";'
 
 
--- webapp/src/main/java/ar/juarce/webapp/auth/CustomBasicAuthenticationFilter.java
+++ webapp/src/main/java/ar/juarce/webapp/auth/CustomBasicAuthenticationFilter.java
@@ -7,24 +7,25 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
-public class BasicAuthFilter extends OncePerRequestFilter {
-
-    private final AuthenticationManager authenticationManager;
+public class BasicAuthFilter extends BasicAuthenticationFilter {
 
     @Autowired
     public BasicAuthFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
+        super(authenticationManager);
     }
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-        // TODO
-        System.out.println("BasicAuthFilter");
+    protected void onSuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, Authentication authResult) throws IOException {
+        super.onSuccessfulAuthentication(request, response, authResult);
+        System.out.println("BasicAuthFilter.onSuccessfulAuthentication called");
     }
 }
