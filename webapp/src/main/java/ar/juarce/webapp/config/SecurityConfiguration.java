f962514f866a77da1e0bc7da3224a75e7f31404f
Julian Arce <52429267+JuArce@users.noreply.github.com>
2023-12-31 19:06:53 -0300

    feat: implement ACL for /users endpoints (#15)

--- webapp/src/main/java/ar/juarce/webapp/config/SecurityConfiguration.java
+++ webapp/src/main/java/ar/juarce/webapp/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package ar.juarce.webapp.config;
 
+import ar.juarce.webapp.auth.AccessControl;
 import ar.juarce.webapp.auth.CustomBasicAuthenticationFilter;
 import ar.juarce.webapp.auth.ForbiddenRequestHandler;
 import ar.juarce.webapp.auth.UnauthorizedRequestHandler;
@@ -8,10 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,16 +31,15 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import java.io.IOException;
-import java.text.ParseException;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
 
 /**
- *  <a href="https://www.baeldung.com/spring-security-basic-authentication">spring-security-basic-authentication</a>
+ * <a href="https://www.baeldung.com/spring-security-basic-authentication">spring-security-basic-authentication</a>
  * <p>
- *  <a href="https://www.baeldung.com/spring-deprecated-websecurityconfigureradapter">spring-deprecated-websecurityconfigureradapter</a>
+ * <a href="https://www.baeldung.com/spring-deprecated-websecurityconfigureradapter">spring-deprecated-websecurityconfigureradapter</a>
  * <p>
- *  <a href="https://www.baeldung.com/role-and-privilege-for-spring-security-registration">role-and-privilege-for-spring-security-registration</a>
+ * <a href="https://www.baeldung.com/role-and-privilege-for-spring-security-registration">role-and-privilege-for-spring-security-registration</a>
  */
 @Configuration
 @EnableWebSecurity
@@ -49,18 +51,21 @@ public class SecurityConfiguration {
     private final JwtDecoder jwtDecoder;
     private final JwtEncoder jwtEncoder;
     private final Converter<Jwt, UsernamePasswordAuthenticationToken> jwtToUserDetailsConverter;
+    private final AccessControl accessControl;
 
     @Autowired
     public SecurityConfiguration(UserDetailsService userDetailsService,
                                  PasswordEncoder passwordEncoder,
                                  JwtDecoder jwtDecoder,
                                  JwtEncoder jwtEncoder,
-                                 Converter<Jwt, UsernamePasswordAuthenticationToken> jwtToUserDetailsConverter) {
+                                 Converter<Jwt, UsernamePasswordAuthenticationToken> jwtToUserDetailsConverter,
+                                 AccessControl accessControl) {
         this.userDetailsService = userDetailsService;
         this.passwordEncoder = passwordEncoder;
         this.jwtDecoder = jwtDecoder;
         this.jwtEncoder = jwtEncoder;
         this.jwtToUserDetailsConverter = jwtToUserDetailsConverter;
+        this.accessControl = accessControl;
     }
 
     @Bean
@@ -82,7 +87,7 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public AuthenticationProvider jwtAuthenticationProvider() throws IOException, ParseException {
+    public AuthenticationProvider jwtAuthenticationProvider() {
         final JwtAuthenticationProvider jwtAuthenticationProvider = new JwtAuthenticationProvider(jwtDecoder);
         jwtAuthenticationProvider.setJwtAuthenticationConverter(jwtToUserDetailsConverter);
         return jwtAuthenticationProvider;
@@ -137,6 +142,11 @@ public class SecurityConfiguration {
 
                 // Set permissions on endpoints
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/users").anonymous()
+                        .requestMatchers(new OrRequestMatcher(
+                                new AntPathRequestMatcher("/users/{id:\\d+}/**", HttpMethod.PUT.toString()),
+                                new AntPathRequestMatcher("/users/{id:\\d+}/**", HttpMethod.DELETE.toString())
+                        )).access((authentication, context) -> new AuthorizationDecision(accessControl.isAuthenticatedUser(authentication.get(), Long.parseLong(context.getVariables().get("id")))))
                         .requestMatchers("/**").permitAll())
 
                 // Add Basic & JWT Authentication filters
